3e07b479e7f8c513c264d28da250591cdd1ae188
Fedele Cavaliere <62469018+feddynventor@users.noreply.github.com>
2024-01-04 23:21:22 +0100

    quick fix drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config();  // Load environment variables from .env file
 // https://orm.drizzle.team/kit-docs/conf
 
 export default {
-  schema: './src/database/schema.ts',
+  schema: './src/core/schema.ts',
   out: './migrations',
   dbCredentials: {
     //drizzle-toolkit entrypoint
